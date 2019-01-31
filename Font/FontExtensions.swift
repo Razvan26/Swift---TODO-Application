4da3c743276b9fe9ed50db5721a8b35d8233e9e4
Razvan26 <34929885+Razvan26@users.noreply.github.com>
2019-01-31 20:01:54 +0200

    Add files via upload

---
+++ Font/FontExtensions.swift
@@ -0,0 +1,18 @@
+//
+//  FontExtensions.swift
+//  TODOList
+//
+//  Created by Razvan26 on 28/01/2019.
+//  Copyright © 2019 Razvan26. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    func myFont (_ size: CGFloat) -> UIFont {
+        let font = UIFont(name: "BradleyHandITCTT-Bold", size: size)!
+        
+        return font
+    }
+}
